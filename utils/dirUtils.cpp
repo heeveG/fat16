786b2443cb9cd918537bb5039b8680eb74f53ef0
Viacheslav Bernadzikovskyi <vbernadzikovskiy@gmail.com>
2020-11-11 23:28:57 +0200

    refact

--- utils/dirUtils.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by Viacheslav Bernadzikovskyi on 11.11.2020.
-//
-
-#include "dirUtils.h"
--- utils/dirUtils.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Viacheslav Bernadzikovskyi on 11.11.2020.
-//
-
-#ifndef FAT16_DIRUTILS_H
-#define FAT16_DIRUTILS_H
-
-#endif //FAT16_DIRUTILS_H
